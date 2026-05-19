0c62d62e912edb9530e61bae908ecb355515a68a
joão Miliano <joaomiliano20@gmail.com>
2026-05-19 00:14:57 -0300

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+int escolha;
+
+
+cout << "===================SENHA==================="  << endl;
+cout << "escolha uma opção :" << endl;
+cout << "1 - Jogar" << endl;
+cout << "2 - Dificuldade" << endl;
+cout << "3 - Sobre" << endl;
+cout << "4 - FIM" << endl;
+cin >> escolha;
+
+switch (escolha)
+{
+case 1:
+    cout << "aaa";
+    break;
+
+case 2:
+    cout << "aaa";
+    break;
+
+case 3:
+    cout << "aaa";
+    break;
+
+
+case 4:
+    return 0;
+
+default:
+    cout << "Digite uma escolha válida";
+    
+}
+
+
+
+
+}
