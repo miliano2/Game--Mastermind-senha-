4669251519d6898200920649ba286e80bd7c85f8
Th1-ag0 <thiagopassos2808@gmail.com>
2026-05-31 22:15:08 -0300

    Correção de errinho

--- code.cpp
+++ code.cpp
@@ -362,24 +362,23 @@ int main() {
 			}
 
 			else if (dificuldade == 3) {
-				int num1, num2, num3,num4, num5, tentativas = 12, res1 = 0, res2 = 0, res3= 0, res4 = 0, res5 = 0;
-				int certos = 0;
-				int errados = 0;
+				int num1, num2, num3,num4, num5, tentativas = 1, res1 = 0, res2 = 0, res3= 0, res4 = 0, res5 = 0;
 				bool resposta = false;
 				srand(time(0));
 
 				do {
-					num1  = rand()%6 + 1;
-					num2  = rand()%6 + 1;
-					num3  = rand()%6 + 1;
-					num4  = rand()%6 + 1;
-					num5  = rand()%6 + 1;
+					num1  = rand()%10 + 1;
+					num2  = rand()%10 + 1;
+					num3  = rand()%10 + 1;
+					num4  = rand()%10 + 1;
+					num5  = rand()%10 + 1;
 				} while (num1 == num2 or num1 == num3 or num1 == num4 or num1 == num5 or num2 == num3 or num2 == num4 or num2 == num5 or num3 == num4 or num3 == num5 or num4 == num5);
 				//cout << num1 << num2 << num3<<num4<<num5;
-				while (tentativas > 0 && resposta == false) {
-					cout <<"============TENTATIVAS RESTANTES: "<< tentativas << " ===================" << endl;
+				while (tentativas <= 12 && resposta == false) {
+					cout << "============DIFICULDADE MÁXIMA===================" << endl;
+					cout << "Tentativa " << tentativas << " de 12" << endl;
 					cout << "Seu último chute foi: " << res1 << res2 << res3 << res4 << res5 << endl;
-					cout << "Digite 5 numeros de 1 a 6 (Aperte enter após cada um deles): " << endl;
+					cout << "Digite 5 numeros de 1 a 10 (Aperte enter após cada um deles): " << endl;
 					cout << "Primeiro numero:  " << endl;
 					cin  >> res1;
 					if(cin.fail()) {
@@ -586,7 +585,7 @@ int main() {
 						cout << "\033[0m";
 					}
 
-					tentativas--;
+					tentativas++;
 
 					if (res1 == num1 && res2 == num2 && res3 == num3 && res4 == num4 && res5 == num5) {
 						system("color 0A");
@@ -598,7 +597,7 @@ int main() {
 
 
 				}
-				if (tentativas == 0 and resposta ==  false) {
+				if (tentativas == 12 and resposta ==  false) {
 					system("color 0C");
 					cout << "Você não conseguiu adivinhar. Que pena!" << endl;
 					cout << "A senha era: " << num1 << num2 << num3 << num4 << num5 << endl;
@@ -660,7 +659,7 @@ int main() {
 			{
 				system("cls");
 				cout << "=========== MODO DUETO ===========" << endl;
-				cout << " Sua Ultima tentativa foi :"<< res1  <<  res2  << res3 << endl;
+				cout << " Sua ultima tentativa foi :"<< res1  <<  res2  << res3 << endl;
 				cout << "Tentativa " << tentativas << " de 15" << endl;
 				cout << endl;
 				if(chave1Resolvida)
@@ -1040,7 +1039,7 @@ int main() {
 			cout << "Equipe de Desenvolvimento==========" << endl;
 			cout << "- João Gabriel M. T. dos Santos" << endl;
 			cout << "- Thiago Schmitt dos Passos" << endl;
-			cout << "- Juliano" << endl;
+			cout << "- Juliano de Souza Castro Vieira" << endl;
 			cout << "- Yasmin Inturn" << endl;
 			cout << "dia e data atuais :" << endl;
 
@@ -1586,7 +1585,7 @@ int main() {
 			}
 
 
-		case 5: // Finaliza o código
+		case 6: // Finaliza o código
 			return 0;
 		}
 		default:
